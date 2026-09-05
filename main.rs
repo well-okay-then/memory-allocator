fd20b57fd9fba0a61cef5af487a0bbafa52af9ba
well-okay-then <ericsoppi@proton.me>
2026-09-04 21:04:33 -0400

    done

--- main.rs
+++ main.rs
@@ -4,9 +4,40 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
+    let mut size = 0;
+    let mut cur = 0;
     for line in stdin.lock().lines() {
         let l = line.unwrap();
-        if l.is_empty() { continue; }
-        println!("TODO");
+        if l.is_empty() {
+            continue;
+        }
+
+        let spl: Vec<&str> = l.split(" ").collect();
+        match spl[0] {
+            "INIT" => {
+                size = spl[1].parse::<i32>().unwrap();
+                println!("OK");
+            }
+            "ALLOC" => {
+                let temp = cur;
+                let t = spl[1].parse::<i32>().unwrap();
+                if t + cur > size {
+                    println!("OOM");
+                } else {
+                    cur += t;
+                    println!("{}", temp);
+                }
+            }
+            "USED" => {
+                println!("{}", cur);
+            }
+            "RESET" => {
+                cur = 0;
+                println!("OK");
+            }
+            _ => {
+                println!("POOP")
+            }
+        }
     }
 }
---
+++ target/debug/deps/memory_allocator-060cfae5f2a4eb9a.d
@@ -0,0 +1,5 @@
+/Users/soppi/workspace/memory-allocator/target/debug/deps/memory_allocator-060cfae5f2a4eb9a.d: main.rs
+
+/Users/soppi/workspace/memory-allocator/target/debug/deps/libmemory_allocator-060cfae5f2a4eb9a.rmeta: main.rs
+
+main.rs:
