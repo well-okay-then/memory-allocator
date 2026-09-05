1a0a8baae10065b27235b7365cac59343885cacf
well-okay-then <ericsoppi@proton.me>
2026-09-04 21:04:52 -0400

    done

--- target/debug/deps/memory_allocator-060cfae5f2a4eb9a.d
+++
@@ -1,5 +0,0 @@
-/Users/soppi/workspace/memory-allocator/target/debug/deps/memory_allocator-060cfae5f2a4eb9a.d: main.rs
-
-/Users/soppi/workspace/memory-allocator/target/debug/deps/libmemory_allocator-060cfae5f2a4eb9a.rmeta: main.rs
-
-main.rs:
--- target/debug/deps/memory_allocator-60d7a0274f8a257c.d
+++
@@ -1,5 +0,0 @@
-/Users/soppi/workspace/memory-allocator/target/debug/deps/memory_allocator-60d7a0274f8a257c.d: main.rs
-
-/Users/soppi/workspace/memory-allocator/target/debug/deps/libmemory_allocator-60d7a0274f8a257c.rmeta: main.rs
-
-main.rs:
